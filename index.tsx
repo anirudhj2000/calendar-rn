8aa9c0e25052ea83733f20241dafd46e2475ae1f
anirudhj2000 <anirudhjoshi985@gmail.com>
2023-10-07 23:20:27 +0530

    initial commit

---
+++ index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View,Text, TouchableOpacity} from 'react-native'
+
+interface CalendarInterface {
+    
+}
+
+const Calendar = () => {
+    return(
+        <View>
+            <Text>Hi</Text>
+        </View>
+    )
+}
+
+export default Calendar;
